ebfd5677872ec4fa3a9a46dee48e29c5f51ceab8
Daniel Myasnikov <myasnikovdaniil@gmail.com>
2013-07-03 14:38:37 +1000

    initial rails commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TestActiveAdmin::Application.config.secret_token = '93d5532fccd9894eb90cd393276733450e3866f7c68c750ef19898b9f3176102511b333dca3e341dcc70e3a9a7890f037bfa9911fee8da5e64031e48e24fe642'
